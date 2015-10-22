5eeda4d03020ea9ceca400c028316e483c021135
Tien Nguyen <nqtien310@gmail.com>
2015-10-22 16:47:21 +0700

    setup testing environment

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,15 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+ENV["RAILS_ENV"] ||= 'test'
+require 'active_support/concern'
+require 'byebug'
+require "rails"
+require 'action_controller/railtie'
+require 'action_view/railtie'
+require 'rspec/rails'
 require 'rails_pattern_view'
+
+
+class Application < Rails::Application
+end
+
+Rails.application.initialize!
