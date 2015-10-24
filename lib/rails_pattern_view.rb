4b257e50673f88d3f38be21dd10cc794e10a28b4
Tien Nguyen <nqtien310@gmail.com>
2015-10-24 16:48:30 +0700

    Implemented

--- lib/rails_pattern_view.rb
+++ lib/rails_pattern_view.rb
@@ -1,5 +1,32 @@
 require "rails_pattern_view/version"
 
 module RailsPatternView
-  # Your code goes here...
+  extend ActiveSupport::Concern
+
+  module ClassMethods
+    def use_pattern(pattern, opts = {})
+      only   = opts[:only]   || []
+      except = opts[:except] || []
+      raise 'cannot mix `only` & `except`' if only.present? && except.present?
+      raise '`only` must be Array type'    if !only.is_a? Array
+      raise '`except` must be Array type'  if !except.is_a? Array
+
+      define_method(:pattern_name) { pattern }
+      private :pattern_name
+
+      #at this point, only & except must be arrays
+      actions = (only.present? ? only : action_methods.to_a) - except
+      before_filter :filter_render, only: actions
+    end
+  end
+
+  private
+
+  def filter_render
+    instance_eval do
+      def default_render
+        render :template => "patterns/#{pattern_name}/#{params[:action]}"
+      end
+    end
+  end
 end
--- spec/rails_pattern_view_spec.rb
+++ spec/rails_pattern_view_spec.rb
@@ -1,11 +1,80 @@
 require 'spec_helper'
 
-describe RailsPatternView do
-  it 'has a version number' do
-    expect(RailsPatternView::VERSION).not_to be nil
+class UsersController < ActionController::Base
+  include RailsPatternView
+end
+
+RSpec.describe UsersController, :type => :controller do
+  controller do
+    def index; end
+    def new; end
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  context '`only` given' do
+    before do
+      controller.class.use_pattern :ajax_table, only: ['index']
+    end
+
+    context 'actions use pattern' do
+      it 'renders patterns' do
+        get :index
+        expect(response).to render_template 'patterns/ajax_table/index'
+      end
+    end
+
+    context 'actions do not use pattern' do
+      it 'renders default' do
+        get :new
+        expect(response).to render_template 'users/new'
+      end
+    end
+  end
+
+  context '`except` given' do
+    before do
+      controller.class.use_pattern :ajax_table, except: ['index']
+    end
+
+    it 'renders patterns for actions not listing in except' do
+      get :index
+      expect(response).to render_template 'users/index'
+
+      get :new
+      expect(response).to render_template 'patterns/ajax_table/new'
+    end
+  end
+
+  context '`only` & `except` are both given' do
+    it 'raises error' do
+      expect {
+        controller.class.use_pattern :ajax_table, only: ['new'], except: ['index']
+      }.to raise_error
+    end
+  end
+
+  context 'wrong type of arguments' do
+    it 'raises error' do
+      expect {
+        controller.class.use_pattern :ajax_table, only: 'new'
+      }.to raise_error
+
+
+      expect {
+        controller.class.use_pattern :ajax_table, except: 'new'
+      }.to raise_error
+    end
+  end
+
+  context '`only` & `except` are not given' do
+    before do
+      controller.class.use_pattern :ajax_table
+    end
+
+    it 'renders patterns for all actions' do
+      get :new
+      expect(response).to render_template 'patterns/ajax_table/new'
+      get :index
+      expect(response).to render_template 'patterns/ajax_table/index'
+    end
   end
 end
